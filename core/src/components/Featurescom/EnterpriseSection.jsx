19751a522d1cacdd878fd8bcbe630d7ec56c2200
Pritirekha25 <pritirekha@nyukt.ai>
2025-07-17 11:33:53 +0530

    issues in ent sec

--- core/src/components/Featurescom/EnterpriseSection.jsx
+++ core/src/components/Featurescom/EnterpriseSection.jsx
@@ -8,7 +8,7 @@ import shield4 from "../../assets/icons/entrp/shield-4.png";
 
 const features = [
   {
-    icon:shield1,
+    icon: shield1,
     title: "Custom AI Models",
     subtitle: "Advanced ML",
     category: "AI/ML",
@@ -24,7 +24,7 @@ const features = [
     color: "purple",
   },
   {
-    icon:shield2,
+    icon: shield2,
     title: "Enterprise Integration",
     subtitle: "Enterprise",
     category: "Integration",
@@ -40,7 +40,7 @@ const features = [
     color: "blue",
   },
   {
-    icon:shield3,
+    icon: shield3,
     title: "Advanced Analytics",
     subtitle: "Analytics Pro",
     category: "Analytics",
@@ -56,7 +56,7 @@ const features = [
     color: "green",
   },
   {
-    icon:shield4,
+    icon: shield4,
     title: "Multi-Language Support",
     subtitle: "Global Scale",
     category: "Localization",
@@ -87,13 +87,15 @@ const EnterpriseSection = () => {
 
       <Grid container spacing={3} className="card-container">
         {features.map((feature, index) => (
-          <Grid item xs={12} sm={6} md={3} key={index}>
+          <Grid item xs={12} sm={6} md={3} key={index}  style={{ display: "flex" }}>
             {/* <Grid className={`top-border ${feature.color}`} /> */}
-            <Card className="feature-card">
+            <Card className="feature-card-ent">
               <div className={`top-border ${feature.color}`} />
               <CardContent>
                 <Grid className="card-header">
-                  <span><img src={feature.icon} className="icon"/></span>
+                  <span>
+                    <img src={feature.icon} className="icon" />
+                  </span>
                   <Grid className="label-chip">
                     <Chip
                       label={feature.category}
@@ -110,7 +112,9 @@ const EnterpriseSection = () => {
                 </Typography>
                 <ul className="feature-lists">
                   {feature.points.map((point, idx) => (
-                    <li key={idx} className="feature-list">{point}</li>
+                    <li key={idx} className="feature-list">
+                      {point}
+                    </li>
                   ))}
                 </ul>
                 <Grid
@@ -118,7 +122,7 @@ const EnterpriseSection = () => {
                     feature.complexity.includes("High") ? "high" : "medium"
                   }`}
                 >
-                  {feature.complexity}
+                  <span className="dots">...</span> {feature.complexity}
                 </Grid>
               </CardContent>
             </Card>
