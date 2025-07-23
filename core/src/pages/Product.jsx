65fe4fba3612113f86f77c57a5696cb9be47a042
Pritirekha25 <pritirekha@nyukt.ai>
2025-07-23 10:36:20 +0530

    product ntytron component sample -1

--- core/src/pages/Product.jsx
+++ core/src/pages/Product.jsx
@@ -1,9 +1,11 @@
 import ProductHero from "../components/Product/ProductHero";
+import NyukTRON from "../components/Product/NyukTRON";
 
 const Product = () => {
   return (
     <>
       <ProductHero/>
+      <NyukTRON/>
     </>
   );
 };
