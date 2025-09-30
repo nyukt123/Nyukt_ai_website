b54974c6a97ae0ede91815c047feb1194460945a
Pritirekha25 <pritirekha@nyukt.ai>
2025-09-30 22:46:35 +0530

    forced commit for rollback

--- core/vite.config.js
+++ core/vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// Test git master
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
